7880351a6837ca06b32e8bbd4187a0dd9bbf83d1
Vasiliy Kabanov <supreme331@yandex.ru>
2023-03-31 15:37:10 +0300

    add gh-pages package

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import {Provider} from "react-redux";
 import {setupStore} from "./store/store";
-import {BrowserRouter} from "react-router-dom";
+import {HashRouter} from "react-router-dom";
 import './styles/index.scss';
 
 const store = setupStore();
@@ -12,9 +12,12 @@ const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 );
 root.render(
-    <BrowserRouter>
+    // Для роутинга использую HashRouter, т.к. приложение хостится на GitHub-Pages, который не умеет определять
+    // точку входа в приложение, при обращении к любому адресу отличающемуся от корневого отдаст 404.
+    // На нормальном хостинге необходимо использовать BrowserRouter.
+    <HashRouter>
         <Provider store={store}>
             <App/>
         </Provider>
-    </BrowserRouter>
+    </HashRouter>
 );
