3a1868563a0cff3c514177c8ac851f38e7bfb7f7
Vasiliy Kabanov <supreme331@yandex.ru>
2023-04-02 22:48:20 +0300

    fix bug with redirect when change type of care

--- src/Components/Catalog/CareTypesBlock.tsx
+++ src/Components/Catalog/CareTypesBlock.tsx
@@ -3,11 +3,18 @@ import styles from "../../styles/CareTypesBlock.module.scss";
 import {showAllProductsOfType} from "../../store/reducers/CatalogSlice";
 import {useAppDispatch, useAppSelector} from "../../store/hooks/redux";
 import {ETypeOfCare} from "../../store/enums/EProducts";
+import {useNavigate} from "react-router";
 
 const CareTypesBlock = () => {
 
+    const navigate = useNavigate();
     const dispatch = useAppDispatch();
     const currentTypeOfCare = useAppSelector(state => state.catalogReducer.currentTypeOfCare);
+
+    function onChangeTypeOfCare(value: ETypeOfCare) {
+        dispatch(showAllProductsOfType(value));
+        navigate('/catalog/1');
+    }
 
     return (
         <ul className={styles.typeOfCare}>
@@ -18,7 +25,7 @@ const CareTypesBlock = () => {
                             className={currentTypeOfCare === value
                                 ? styles.typeOfCareActiveItem
                                 : styles.typeOfCareItem}
-                        onClick={() => dispatch(showAllProductsOfType(value))}>
+                        onClick={() => onChangeTypeOfCare(value)}>
                             {value}
                         </li>)
             }
--- src/Components/Catalog/CatalogAside/AsideSection.tsx
+++ src/Components/Catalog/CatalogAside/AsideSection.tsx
@@ -3,10 +3,12 @@ import styles from '../../../styles/AsideSection.module.scss';
 import {useAppDispatch, useAppSelector} from "../../../store/hooks/redux";
 import {setCurrentSubtypeOfCare, showAllProductsOfType} from "../../../store/reducers/CatalogSlice";
 import {ETypeOfCare} from "../../../store/enums/EProducts";
+import {useNavigate} from "react-router";
 
 
 const AsideSection: React.FC<AsideSectionProps> = ({title, subTypeOfCareEnum}) => {
 
+    const navigate = useNavigate();
     const dispatch = useAppDispatch();
     const currentTypeOfCare = useAppSelector(state => state.catalogReducer.currentTypeOfCare);
     const currentSubtypeOfCare = useAppSelector(state => state.catalogReducer.currentSubtypeOfCare);
@@ -16,9 +18,14 @@ const AsideSection: React.FC<AsideSectionProps> = ({title, subTypeOfCareEnum}) =
         setIsActive(currentTypeOfCare === title)
     },[currentTypeOfCare, title])
 
+    function onChangeTypeOfCare(value: ETypeOfCare) {
+        dispatch(showAllProductsOfType(value));
+        navigate('/catalog/1');
+    }
+
     return (
         <div>
-            <h3 onClick={() => dispatch(showAllProductsOfType(title))}
+            <h3 onClick={() => onChangeTypeOfCare(title)}
                 className={isActive ? styles.activeTitle : styles.title}>{title}</h3>
             <ul>
                 {
@@ -26,7 +33,10 @@ const AsideSection: React.FC<AsideSectionProps> = ({title, subTypeOfCareEnum}) =
                         <li  className={currentSubtypeOfCare === value
                             ? styles.subtypeActiveItem
                             : styles.subtypeItem}
-                             onClick={() => dispatch(setCurrentSubtypeOfCare(value))}
+                             onClick={() => {
+                                 dispatch(setCurrentSubtypeOfCare(value));
+                                 navigate('/catalog/1');
+                             }}
                              key={value}>{value}</li>)
                 }
             </ul>
